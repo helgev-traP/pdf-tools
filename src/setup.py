aefe5f0116ad8d610fe5850758249d29affaee3c
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-11-22 22:59:22 +0900

    log

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="pdf-tools",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "img2pdf",
+        "pdf2iage",
+        "opencv-python",
+    ],
+)
